d407ae3f929dea303624c5ba59f35c9377e10506
lazy-sky <1cielo@naver.com>
2022-02-01 02:19:45 +0900

    refact

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -6,6 +6,7 @@
 */
 
 import * as postsAPI from '../api/posts';
+import { createPromiseThunk, reducerUtils, handleAsyncActions } from '../lib/asyncUtils';
 
 // 액션 타입
 
@@ -19,93 +20,28 @@ const GET_POST = 'GET_POST';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
 
-// thunk를 사용할 때 꼭 모든 액션들에 대해 액션 생성함수를 만들 필요는 없다.
-// thunk 함수에서 바로 액션 객체를 만들어줘오 괜찮다.
-export const getPosts = () => async (dispatch) => {
-  dispatch({ type: GET_POSTS }); // 요청 시작
-  try {
-    const posts = await postsAPI.getPosts();
-    dispatch({ type: GET_POSTS_SUCCESS }); // 요청 성공
-  } catch (e) {
-    dispatch({ type: GET_POST_ERROR, error: e }); // 실패
-  }
-};
+export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
+export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
 
 const initialState = {
-  posts: {
-    loading: false,
-    data: null,
-    error: null
-  },
-  post: {
-    loading: false,
-    data: null,
-    error: null
-  }
+  posts: reducerUtils.initial(),
+  post: reducerUtils.initial()
 };
 
 export default function posts(state = initialState, action) {
   switch (action.type) {
     case GET_POSTS:
-      return {
-        ...state,
-        posts: {
-          loading: true,
-          data: null,
-          error: null
-        }
-      };
-
     case GET_POSTS_SUCCESS:
-      return {
-        ...state,
-        posts: {
-          loading: true,
-          data: action.posts,
-          error: null
-        }
-      };
-
     case GET_POSTS_ERROR:
-      return {
-        ...state,
-        posts: {
-          loading: true,
-          data: null,
-          error: action.error
-        }
-      };
-
+      return handleAsyncActions(GET_POSTS, 'posts')(state, action);
+      // 혹은 아래와 같이 표현할 수도 있다.
+      // const postsReducer = handleAsyncActions(GET_POSTS, 'posts');
+      // return postsReducer(state, action);
     case GET_POST:
-      return {
-        ...state,
-        post: {
-          loading: true,
-          data: null,
-          error: null
-        }
-      };
-
     case GET_POST_SUCCESS:
-      return {
-        ...state,
-        post: {
-          loading: true,
-          data: action.post,
-          error: null
-        }
-      };
-
     case GET_POST_ERROR:
-      return {
-        ...state,
-        post: {
-          loading: true,
-          data: null,
-          error: action.error
-        }
-      };
-      
+      return handleAsyncActions(GET_POST, 'post')(state, action);
+
     default:
       return state;
   }
